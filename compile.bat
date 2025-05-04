8f11fdaf094cdbbec3b8414112f55f305f25b96a
x.wang <xi.wang@riedel.net>
2025-05-04 11:30:13 +0800

    Clean up

--- compile.bat
+++ compile.bat
@@ -1,16 +1,9 @@
-@echo off
-set BASEDIR=D:\Project\3rd\test-grpc\
+@ECHO ON
+
+set BASEDIR=%~dp0
 PUSHD %BASEDIR%
 
-REM 首先确保生成protobuf文件
-cd src
-call compile_protos.bat
-cd ..
-
-REM 清理build目录
 RMDIR /Q /S build
 
-REM 使用conan构建项目
-conan build . --output-folder=build\release --build=missing -s build_type=Release
-
-POPD
+conan build . --output-folder=build --build=missing -s build_type=Debug
+@REM conan build . --output-folder=build --build=missing -s build_type=Release
--- protos/compile_protos.bat
+++ protos/compile_protos.bat
@@ -1,6 +1,10 @@
-C:\Users\icegu\.conan2\p\b\proto02f307ad8f5b1\b\build\Release\protoc.exe --cpp_out=. --proto_path=..\protos\ helloworld.proto
+C:\Users\icegu\.conan2\p\b\proto02f307ad8f5b1\b\build\Release\protoc.exe ^
+--cpp_out=..\src ^
+--proto_path=.\ ^
+helloworld.proto
 
 C:\Users\icegu\.conan2\p\b\proto02f307ad8f5b1\b\build\Release\protoc.exe ^
- --grpc_out=. ^
+ --grpc_out=..\src ^
  --plugin=protoc-gen-grpc=C:\Users\icegu\.conan2\p\b\grpc149fa6a9f0f43\b\build\Release\grpc_cpp_plugin.exe ^
- --proto_path=..\protos\ helloworld.proto
+ --proto_path=.\ ^
+helloworld.proto
